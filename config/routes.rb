a0f731161dfb22d3fe6dc8511521947f95a60a64
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2025-05-31 15:43:29 +0200

    session: ajout du login
    Le bouton de connexion envoie sur `POST /auth/proconnect` qui
    délenchera une redirection vers ProConnect calculée par la stratégie.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get "home/index"
+
+  get "/login", as: :login, to: "sessions#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
