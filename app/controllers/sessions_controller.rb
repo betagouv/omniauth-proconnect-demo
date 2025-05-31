0a11667a23ff09ec9813fd7c23948d616191aac0
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2025-05-31 15:44:00 +0200

    sessions: ajout de l'URL de retour de ProConnect
    une fois l'utilisateur authentifié sur ProConnect, il est redirigé
    vers notre serveur grâce à la `redirect_uri` (ici
    `http://localhost:3000/auth/proconnect/callback?code=machin`). La
    stratégie intercepte, utilise le code pour échanger des infos avec
    ProConnect qu'elle met dans `request.env["omniauth.hash"], puis rend
    la main à votre stack Rails : il faut donc mapper cette URl quelque
    part.
    
    Ici, on créé ou on initialise un utilisateur avec le `uid`, puis on
    met à jour l'email, et si quelque chose se passe mal on redirige en
    exposant les données (ne pas faire ça en production).

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    data = request.env["omniauth.hash"]
+
+    @user = User.find_or_initialize_by(data.uid) do |user|
+      user.email = data.info.email
+    end
+
+    if @user.save
+      redirect_to root_path, notice: "Connexion réussie pour #{@user.email}"
+    else
+      redirect_to root_path, notice: "Erreur de connexion : #{data}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "home/index"
 
   get "/login", as: :login, to: "sessions#new"
+  get "/auth/proconnect/callback", to: "sessions#create"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
